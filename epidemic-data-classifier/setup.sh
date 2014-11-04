2f9becf89d1ad76b70ce22b44761c0dea1572c97
Denis Costa <deniscostadsc@gmail.com>
2014-11-03 23:35:19 -0200

    Initial commit.

---
+++ epidemic-data-classifier/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd $(dirname $0) && cd edc
+
+if [[ -z $VIRTUAL_ENV ]]; then
+    echo "You should start your virtualenv to setup the project!"
+    exit 1
+fi
+
+# Create a syslink to be able to upload libs to appengine
+ln -s $VIRTUAL_ENV/lib/python?.?/site-packages lib
